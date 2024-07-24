7d3adad29dd379e4eea20a57e2d73795567b05cd
Ana Manica <ana@manica.org>
2024-07-24 15:47:31 -0600

    I am a God

--- imap_processing/tests/testing_corrupt_cdf.py
+++ imap_processing/tests/testing_corrupt_cdf.py
@@ -1,19 +1,24 @@
 import os
+from pathlib import Path
 
 import numpy as np
 import pandas as pd
 import xarray as xr
-from cdflib.xarray import xarray_to_cdf
+from cdflib.xarray import cdf_to_xarray, xarray_to_cdf
 
-# COMPLEX EXAMPLE USAGE
-# # Create CDF Object and stuff
-# idex_attrs = ImapCdfAttributes()
-# idex_attrs.add_instrument_global_attrs("idex")
-# idex_attrs.add_instrument_variable_attrs("idex", "l1")
-# attrs = idex_attrs.get_global_attributes(
-#     "imap_idex_l1_sci"
-# ) | idex_attrs.get_variable_attributes("sample_rate_base")
-# print(attrs)
+from imap_processing.cdf.imap_cdf_manager import ImapCdfAttributes
+from imap_processing.cdf.utils import load_cdf, met_to_j2000ns, write_cdf
+
+IMAP_EPOCH = np.datetime64("2010-01-01T00:00:00", "ns")
+
+# Create CDF Object and stuff
+idex_attrs = ImapCdfAttributes()
+idex_attrs.source_dir = Path(__file__).parent.parent / "tests"
+idex_attrs.add_instrument_global_attrs("idex")
+idex_attrs.add_instrument_variable_attrs("idex", "l1")
+logical_source = idex_attrs.get_global_attributes("imap_idex_l1_sci")
+attrs = idex_attrs.get_variable_attributes("sample_test", False)
+print("ATTRS: ", attrs)
 
 # Create some example data
 temperature = np.random.rand(5, 3) * 30  # 5 days, 3 locations
@@ -23,29 +28,60 @@ precipitation = np.random.rand(5, 3) * 100  # 5 days, 3 locations
 times = pd.date_range("2024-07-01", periods=5)
 locations = ["Location1", "Location2", "Location3"]
 
-# Creating a stupid string
-# with open("beeMovie.txt") as file:
-#     beeMovie = file.read().replace("\n", " ")
+# Convert times to CDF_EPOCH
+times_seconds = [(t - IMAP_EPOCH).total_seconds() for t in times]
+epoch_time = met_to_j2000ns(times_seconds)  # These are int64 type
+
+print("epoch_time type: ", type(epoch_time[0]))
+print("HERE: ", type(epoch_time))
+
+istp_dict = {
+    "VAR_TYPE": "data",
+    "Project": "Project Input",
+    "CATDESC": "Catdesc input",
+    "DISPLAY_TYPE": "no_plot",
+    "LABLAXIS": "Label",
+    "FIELDNAM": "Field",
+    "FORMAT": "I10",
+    "UNITS": "seconds",
+    "VALIDMIN": "-130",
+    "VALIDMAX": "130",
+    "FILLVAL": "-9223372036854775808",
+}
+
+epoch_dict = {
+    "VAR_TYPE": "support_data",
+    "CATDESC": "Time, number of nanoseconds since J2000 with leap seconds included",
+    "FIELDNAM": "epoch",
+    "FILLVAL": -9223372036854775808,
+    "LABLAXIS": "epoch",
+    "FORMAT": "",
+    "UNITS": "ns",
+    "VALIDMIN": -315575942816000000,
+    "VALIDMAX": 3155630469184000000,
+    "SCALETYP": "linear",
+    "MONOTON": "INCREASE",
+    "TIME_BASE": "J2000",
+    "TIME_SCALE": "Terrestrial Time",
+    "REFERENCE_POSITION": "Rotating Earth Geoid",
+    "DISPLAY_TYPE": "no_plot",
+}
+
+epoch = xr.DataArray(epoch_time, name="epoch", dims=["epoch"], attrs=epoch_dict)
+location = xr.DataArray(locations, name="location", dims=["location"], attrs=istp_dict)
 
 # Create the dataset
 data = xr.Dataset(
     {
-        "temperature": (["time", "location"], temperature),
-        "precipitation": (["time", "location"], precipitation),
+        "temperature": (["epoch", "location"], temperature, istp_dict),
+        "precipitation": (["epoch", "location"], precipitation, istp_dict),
     },
-    coords={
-        "time": times,
-        "location": locations,
-    },
-    # attrs=attrs, COMPLEX
-    attrs={"FILLVAL": "-1.0e31", "VALIDMAX": 130},
+    coords={"epoch": epoch, "location": location},
+    attrs=attrs,
 )
 
-# Ensure all attribute values are strings to avoid issues
-for attr in data.attrs:
-    data.attrs[attr] = str(data.attrs[attr])
-
 print(data)
+print(data.coords)
 
 # File name
 file_name = "example_file.cdf"
@@ -54,6 +90,7 @@ file_name = "example_file.cdf"
 if os.path.exists(file_name):
     os.remove(file_name)
 
+# Using base functions
 xarray_to_cdf(
     data,
     file_name,
@@ -66,14 +103,8 @@ xarray_to_cdf(
     nan_to_fillval=True,
     from_unixtime=False,
     from_datetime=False,
-    unixtime_to_cdftt2000=False,
-    datetime_to_cdftt2000=True,
-    datetime64_to_cdftt2000=True,
 )
+cdf_to_xarray(file_name)
 
-# cdf_to_xarray(file_name)
-print("Passed gen functions")
-
-# COMPLEX USAGE
-# cdf_file = write_cdf(data)
-# load_cdf(cdf_file)
+file_cdf = write_cdf(data)
+load_cdf(file_cdf)
